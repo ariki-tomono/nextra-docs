8bd2e9c1fe2215ba8092fa8327bd93d0b5772027
Ariki Tomono <ariki@tomono.co.jp>
2023-04-06 16:32:21 +0900

    仕様の各ページを一通り作成完了

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,13 +3,13 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>岡山大学卒後臨床研修センター　協力型病院検索システム</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: '岡山大学卒後臨床研修センター　協力型病院検索システム',
   },
