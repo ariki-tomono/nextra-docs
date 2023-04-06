3cd20e46915f41d59a291952d2948920ef5cc192
Ariki Tomono <ariki@tomono.co.jp>
2023-04-06 13:14:12 +0900

    仕様部分のページ構成作成

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>プロジェクトタイトル</span>,
+  logo: <span>岡山大学卒後臨床研修センター　協力型病院検索システム</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '岡山大学卒後臨床研修センター　協力型病院検索システム',
   },
 }
 
